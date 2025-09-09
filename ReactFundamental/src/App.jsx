565c3646adcfc8711685bda58906ea673ade7de5
zaldighar <zaldighar@outlook.com>
2025-09-09 08:56:01 -0500

    feat: add HomePage, AboutPage, and ContactPage components; update dependencies in package.json and package-lock.json

--- ReactFundamental/src/App.jsx
+++ ReactFundamental/src/App.jsx
@@ -1,22 +1,10 @@
-const App = () => {
-  const city = [
-    "Dhaka",
-    "Chittagong",
-    "Khulna",
-    "Barishal",
-    "Sylhet",
-    "Rajshahi",
-  ];
 
+
+
+const App = () => {
   return (
     <div>
-      {
-        <ul style={{  }}>
-          {city.map((c) => (
-            <li key={c} style={{ marginBottom: '5px', listStyleType: 'none', color: 'blue' }}>{c}</li>
-          ))}
-        </ul>
-      }
+   
     </div>
   );
 };
